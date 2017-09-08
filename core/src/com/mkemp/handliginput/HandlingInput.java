a0b1439ca40d1e0e8c0640fb224e012bebfe58cd
Michael Kemp <mkemp@accu-time.com>
2017-09-08 16:28:41 -0400

    Initial commit

---
+++ core/src/com/mkemp/handliginput/HandlingInput.java
@@ -0,0 +1,55 @@
+package com.mkemp.handliginput;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class HandlingInput extends ApplicationAdapter {
+	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;
+	
+	@Override
+	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		batch = new SpriteBatch();
+
+		texture = new Texture("0001.png");
+		sprite = new Sprite(texture);
+		sprite.setPosition(w/2 - sprite.getWidth()/2, h/2 - sprite.getHeight()/2);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(-1f);
+			else
+				sprite.translateX(-10f);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT))
+				sprite.translateX(1f);
+			else
+				sprite.translateX(10f);
+		}
+
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		texture.dispose();
+	}
+}
